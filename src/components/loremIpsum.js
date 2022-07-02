e8ad663abc579fcf54ab45f40ef4bc6eec62c0c9
Raul Correia <raulvinicius245@gmail.com>
2022-07-01 22:43:08 -0300

    Component com paragrafo.

---
+++ src/components/loremIpsum.js
@@ -0,0 +1,12 @@
+const lorem = () => {
+    return(
+        <p>
+            Mussum Ipsum, cacilds vidis litro abertis. Viva Forevis aptent taciti sociosqu ad
+            litora torquent.Mais vale um bebadis conhecidiss, que um alcoolatra anonimis.Interagi
+            no mé, cursus quis, vehicula ac nisi.Nullam volutpat risus nec leo commodo, ut interdum
+            diam laoreet. Sed non consequat odio.
+        </p>
+    )
+}
+
+export default lorem;
