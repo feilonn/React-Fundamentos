8662cf57072196af070969d238d03aff7ab415dc
Raul Correia <raulvinicius245@gmail.com>
2022-07-05 18:53:29 -0300

    Controlando multiplos inputs

--- src/components/planets/form/index.js
+++ src/components/planets/form/index.js
@@ -1,22 +1,44 @@
 import { useState, useEffect } from "react";
 
+const initialState = {
+    name: '',
+    description: '',
+    link: '',
+    img_url: ''
+}
+
 const Form = (props) => {
-    const [planetName, setName] = useState("");
-    const handleChange = event => setName(event.target.value);
+    const [fields, setFields] = useState(initialState);
+    
+    const handleFieldsChange = event => setFields({
+        ...fields,
+        [event.target.name]: event.target.value
+    });
+
     const handleSubmit = event => {
         event.preventDefault();
-        props.addPlanet({name: planetName});
+        props.addPlanet(fields);
+        setFields(initialState);
     }
 
-    useEffect(() => {
-        setName()
-    }, [])
+    // useEffect(() => {
+    //     setName()
+    // }, [])
 
     return (
         <>
             <form onSubmit={handleSubmit}>
                 <label htmlFor="planetName">Nome do Planeta: </label>
-                <input type="text" id="planetName" value={planetName} onChange={handleChange}/>
+                <input type="text" id="planetName" name="name" value={fields.name} onChange={handleFieldsChange}/>
+                <br/>
+                <label htmlFor="planetName">Descrição: </label>
+                <input type="text" id="planetName" name="description" value={fields.description} onChange={handleFieldsChange}/>
+                <br/>
+                <label htmlFor="planetName">Link: </label>
+                <input type="text" id="planetName" name="link" value={fields.link} onChange={handleFieldsChange}/>
+                <br/>
+                <label htmlFor="planetName">URL Image: </label>
+                <input type="text" id="planetName" name="img_url" value={fields.img_url} onChange={handleFieldsChange}/>
                 <br/>
                 <input type="submit"/>
             </form>
